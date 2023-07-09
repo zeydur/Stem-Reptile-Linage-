2676077792fb26659dfc67c2620b64c8197b8925
zeydur <118747089+zeydur@users.noreply.github.com>
2023-07-09 15:35:25 +0100

    Create orthofinder.sh

---
+++ orthofinder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=orthtrytestfinal
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --mem=179g
+#SBATCH --time=72:00:00
+#SBATCH --output=/gpfs01/home/mbxzd1/OandE/%x.out
+#SBATCH --error=/gpfs01/home/mbxzd1/OandE/%x.err
+
+# These steps are required to activate Conda
+source $HOME/.bash_profile
+conda activate orthofinder
+
+#After activation run the orthofinder with default parameters
+orthofinder -d -f /gpfs01/home/mbxzd1/testfinal/
