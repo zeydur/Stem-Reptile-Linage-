bca52f0275930d9eab91cfbe68329b506ec51291
zeydur <118747089+zeydur@users.noreply.github.com>
2023-07-20 10:53:06 +0100

    Update eggnogOG24.sh

--- eggnogOG24.sh
+++ eggnogOG24.sh
@@ -22,3 +22,44 @@ python3 /gpfs01/home/mbxzd1/miniconda3/envs/eggnog_env/bin/emapper.py \
     -o output_folderOG24 \
     --data_dir /gpfs01/home/mbxzd1/eggnog_data/ \
     --dbtype seqdb
+
+python3 /gpfs01/home/mbxzd1/miniconda3/envs/eggnog_env/bin/emapper.py \
+    -i /gpfs01/home/mbxzd1/testfinal/OrthoFinder/Results_Jun28/Orthogroup_Sequences/OG0000074_protein.fa \
+    -o output_folderOG74 \
+    --data_dir /gpfs01/home/mbxzd1/eggnog_data/ \
+    --dbtype seqdb
+
+python3 /gpfs01/home/mbxzd1/miniconda3/envs/eggnog_env/bin/emapper.py \
+    -i /gpfs01/home/mbxzd1/testfinal/OrthoFinder/Results_Jun28/Orthogroup_Sequences/OG0000141_protein.fa \
+    -o output_folderOG141 \
+    --data_dir /gpfs01/home/mbxzd1/eggnog_data/ \
+    --dbtype seqdb
+
+python3 /gpfs01/home/mbxzd1/miniconda3/envs/eggnog_env/bin/emapper.py \
+    -i /gpfs01/home/mbxzd1/testfinal/OrthoFinder/Results_Jun28/Orthogroup_Sequences/OG0000213_protein.fa \
+    -o output_folderOG213 \
+    --data_dir /gpfs01/home/mbxzd1/eggnog_data/ \
+    --dbtype seqdb
+
+python3 /gpfs01/home/mbxzd1/miniconda3/envs/eggnog_env/bin/emapper.py \
+    -i /gpfs01/home/mbxzd1/testfinal/OrthoFinder/Results_Jun28/Orthogroup_Sequences/OG0000307_protein.fa \
+    -o output_folderOG307 \
+    --data_dir /gpfs01/home/mbxzd1/eggnog_data/ \
+    --dbtype seqdb
+python3 /gpfs01/home/mbxzd1/miniconda3/envs/eggnog_env/bin/emapper.py \
+    -i /gpfs01/home/mbxzd1/testfinal/OrthoFinder/Results_Jun28/Orthogroup_Sequences/OG0000359_protein.fa \
+    -o output_folderOG359 \
+    --data_dir /gpfs01/home/mbxzd1/eggnog_data/ \
+    --dbtype seqdb
+
+python3 /gpfs01/home/mbxzd1/miniconda3/envs/eggnog_env/bin/emapper.py \
+    -i /gpfs01/home/mbxzd1/testfinal/OrthoFinder/Results_Jun28/Orthogroup_Sequences/OG0000700_protein.fa \
+    -o output_folderOG700 \
+    --data_dir /gpfs01/home/mbxzd1/eggnog_data/ \
+    --dbtype seqdb
+
+python3 /gpfs01/home/mbxzd1/miniconda3/envs/eggnog_env/bin/emapper.py \
+    -i /gpfs01/home/mbxzd1/testfinal/OrthoFinder/Results_Jun28/Orthogroup_Sequences/OG0000849_protein.fa \
+    -o output_folderOG849 \
+    --data_dir /gpfs01/home/mbxzd1/eggnog_data/ \
+    --dbtype seqdb
